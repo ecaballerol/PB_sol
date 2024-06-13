9d68fa62bc67710e1cc9e6bacd7feaee0f98e225
ecaballerol <caballero2691@gmail.com>
2024-06-13 17:33:49 +1200

    Jupyter

--- StaSLip.py
+++ StaSLip.py
@@ -1,16 +1,6 @@
-<<<<<<< HEAD
-
-=======
-#%%
->>>>>>> dd39d1e (Initialize)
 # Rapid Estimation of Slip 
 
 #Created by E.Caballero GNS
 
 import numpy as np
-<<<<<<< HEAD
 import matplotlib.pyplot as plt
-=======
-import matplotlib.pyplot as plt
-
->>>>>>> dd39d1e (Initialize)
