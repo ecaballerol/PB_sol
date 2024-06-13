82cdec3c71ae4f4f4daa33cff08432ecf3f99ffa
ecaballerol <caballero2691@gmail.com>
2024-06-13 17:52:46 +1200

    Looking for GNSS

--- StaSLip.py
+++ StaSLip.py
@@ -6,3 +6,23 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+import os
+
+#------------------------------------------------------------------
+#------------------------------------------------------------------
+# Load Arguments
+from Arguments import *
+
+#%%
+#Searching Available DATA
+
+if os.listdir(gps_dir):
+    print('GNS')
+else:
+    print('notGNS')
+
+#Loading data
+
+# %%
+#Initialize fault object
+
