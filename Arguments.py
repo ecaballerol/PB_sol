1f192028404aafe24d1f14b93cfc84aa11a329da
ecaballerol <caballero2691@gmail.com>
2024-06-17 10:07:38 +1200

    Starting with Fault Geometry

--- Arguments.py
+++ Arguments.py
@@ -5,9 +5,20 @@ from os.path import join,exists,basename
 import math
 from glob import glob
 
+#Define the UTM
+utmzone = 19
 
 #DATA dir
 
 gps_dir = '../DATA/GNSS'
 
 print('tete')
+
+#+++++++++++++++++++++++++
+# Fault Geometry Parameters
+
+TopEdge = {'lon': -72.419,'lat':-32.25,'depth':10} 
+FaultGeo = {'length':300,'width':180,\
+            'strike':4,'dip':22,\
+            'n_strike':10,'n_dip':10,\
+            'grid_size':2}
--- StaSLip.py
+++ StaSLip.py
@@ -19,6 +19,7 @@ import csi.seismic    as seis
 import csi.RectangularPatchesKin as rectangular_fault # Use only for kinematic modeling
 import csi.multifaultsolve as multiflt
 import csi.geodeticplot as geoplt
+import csi.planarfaultkinematic as planar_fault
 
 #------------------------------------------------------------------
 #------------------------------------------------------------------
@@ -26,15 +27,26 @@ import csi.geodeticplot as geoplt
 from Arguments import *
 
 #%%
-#Searching Available DATA
-
+#Searching and loading Available DATA
+data_avail = []
 if os.listdir(gps_dir):
     print('GNS')
+    GPS = gr('GPS',utmzone=utmzone)
+    GPS.read_from_enu(gps_dir,header=1)#,factor=1000.)
+    GPS.buildCd(direction='enu')
 else:
     print('notGNS')
 
+#Other approach
+# for idir in os.listdir('DATA'):
+#     print(idir)
 #Loading data
 
 # %%
 #Initialize fault object
+# Two approaches, either Initialize planar fault, either 
+#look for the geometry
+# Initialize a planar fault
+fault = planar_fault('EarSlInv', utmzone=utmzone)
+
 
