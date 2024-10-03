aa55f623bbc5037718e75df67bde2dce348060e3
Emmax <caballero2691@com>
2024-10-03 16:20:54 +1300

    Bug geometry

--- StaSLip.py
+++ StaSLip.py
@@ -65,8 +65,8 @@ else:
     FaultGeo['length']=length
     FaultGeo['width'] = width
     #if FaultGeo['strike'] >0 and FaultGeo['strike'] < 90:
-    Toplon = lon_hypo - (FaultGeo['width']/2 *np.cos(np.deg2rad(strike)))/111 
-    Toplat = lat_hypo + (FaultGeo['width']/2 *np.sin(np.deg2rad(strike)))/111 
+    Toplon = lon_hypo - (FaultGeo['width']/2 *np.cos(np.deg2rad(strike))*np.cos(np.deg2rad(dip)))/111 
+    Toplat = lat_hypo + (FaultGeo['width']/2 *np.sin(np.deg2rad(strike))*np.cos(np.deg2rad(dip)))/111 
     #elif FaultGeo['strike']>=90 and FaultGeo['strike']<180:
     TopEdge['lon']=Toplon
     TopEdge['lat']=Toplat
